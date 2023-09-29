7986362c31402624e66a7c54788a96c57d2a692c
Anders Gorm Pedersen <agpe@dtu.dk>
2023-09-29 14:03:48 +0200

    fix: include header file for std::string

--- src/data.hpp
+++ src/data.hpp
@@ -4,17 +4,17 @@
  *
  * http://becs.aalto.fi/en/research/bayes/bmagwa/
  * Copyright 2012 Tomi Peltola <tomi.peltola@aalto.fi>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -25,6 +25,7 @@
 
 #include <cmath>
 #include <map>
+#include <string>
 #include "linalg.hpp"
 
 namespace bmagwa {
